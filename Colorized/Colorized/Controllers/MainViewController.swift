190b82d58b387e92bc9ae3792b005fd97db7a6f9
Nikolai Maksimov <kolkamaximov@gmail.com>
2023-12-23 17:50:48 +0300

    добавлен экран (MainViewController) отображения выбранного цвета

---
+++ Colorized/Colorized/Controllers/MainViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MainViewController.swift
+//  Colorized
+//
+//  Created by Nikolai Maksimov on 23.12.2023.
+//
+
+import UIKit
+
+protocol ColorViewControllerDelegate: AnyObject {
+    func setColor(_ color: UIColor)
+}
+
+final class MainViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       commonInit()
+    }
+    
+}
+
+// MARK: Metods
+extension MainViewController {
+    private func commonInit() {
+        view.backgroundColor = .lightGray
+        addBarButtonItem()
+    }
+    
+    private func addBarButtonItem() {
+//        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: ., target: self, action: #selector(showColorViewController))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: nil, image: UIImage(systemName: "rectangle.portrait.and.arrow.forward"), target: self, action: #selector(showColorViewController))
+    }
+    
+    @objc
+    private func showColorViewController() {
+        let colorViewController = ColorViewController()
+        colorViewController.viewColor = view.backgroundColor
+        colorViewController.delegate = self
+        navigationController?.pushViewController(colorViewController, animated: true)
+    }
+}
+
+// MARK: ColorViewControllerDelegate
+extension MainViewController: ColorViewControllerDelegate {
+    
+    func setColor(_ color: UIColor) {
+        view.backgroundColor = color
+    }
+}
